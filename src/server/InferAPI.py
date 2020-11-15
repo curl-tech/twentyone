f66cf0112cf91f3e927a01c1b442d0031813c4fd
mohit6b <mohit6b@gmail.com>
2020-11-15 17:54:56 +0530

    Added Infer API

--- src/server/InferAPI.py
+++ src/server/InferAPI.py
@@ -1,9 +1,10 @@
 import yaml
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from base64 import b64decode, b64encode
 
 import sys
 sys.path.append("..")
-
+import pandas as pd
 from tasks.tasks import Task
 from data.data import Data
 from models.model import Model
@@ -14,10 +15,22 @@ config_path = "config/config.yaml"
 with open(config_path, "r") as fp:
     config = yaml.load(fp, Loader=yaml.FullLoader)
 
-@infer_api.route("/infer")
+@infer_api.route("/infer", methods = ['GET'])
 def infer():
     # in the response you get task id and data, convert data to right format
-    model = Model()
-    location = config["model"]["save_location"]
-    res = model.infer(location, "task.id", "data")
-    return res
+    request_method = request.method
+
+    if request_method == "GET":
+        file = request.files['file']
+        # task_id = request.files['task_id']
+        foo=file.filename
+        print("File Name : ", foo)
+        unparsedFile = file.read()
+        dframe = pd.read_excel(file, index_col="date")
+        data_config = jsonify(request.data)
+        file_data = unparsedFile
+
+        model = Model()
+        location = config["model"]["save_location"]
+        res = model.infer(location, "okP0KEPL", dframe)
+    return str(res)
