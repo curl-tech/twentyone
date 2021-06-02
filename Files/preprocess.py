edad52a19d89e50d66c5f242ecaeb38b97a078c7
Shubham2443 <sks2443.1947@gmail.com>
2021-06-03 04:43:24 +0530

    Created the necessery methods

---
+++ Files/preprocess.py
@@ -0,0 +1,83 @@
+from main import *
+from pycaret.classification import *
+from pycaret.regression import *
+# from pycaret.clustering import *
+# from pycaret.nlp import *
+import os
+
+
+class Preprocess:
+    def auto_preprocess(model_type,raw_data_address,target_variable):
+        """
+        This function is for preprocessing the data when the user selects auto preprocessing.
+    
+        Extended description of function.
+    
+        Parameters:
+            model_type (string): The selection of the user for the model_type eg-classification/regression/clustering etc.
+            raw_data_address (string): As the user uploads the raw dataset it will be saved in the database, this is the address of that saved dataset.
+            target_variable (string): The user will select the target cloumn/variable and that target variable name have to be passed to the setup() in pycaret as patameter.
+            
+        
+        Returns:
+            clean_data_address(string): 
+        
+        """
+  
+
+        if model_type == "classification":
+            clf1 = setup(data = raw_data_address, target = target_variable,silent=True, profile= True)
+        
+        elif model_type == "regression":
+            reg1 = setup(data = raw_data_address, target = target_variable,silent=True, profile= True)
+
+        # elif model_type == "clustering":            
+        #     clu1 = setup(data = raw_data_address,silent=True, profile= True)
+
+        # elif model_type == "nlp":            
+        #     nlp1 = setup(data = raw_data_address, target = target_variable,silent=True, profile= True)
+        
+        X_train = get_config('X_train')        
+        
+        X_train.to_csv('clean_data.csv', index=False)
+        
+        clean_data_address = os.getcwd()+"/clean_data.csv"
+        return clean_data_address
+        
+    def manual_preprocess(model_type,raw_data_address,target_variable,parameters):
+        """
+        This function is for preprocessing the data when the user selects manual preprocessing.
+    
+        Extended description of function.
+    
+        Parameters:
+            model_type (string): The selection of the user for the model_type eg-classification/regression/clustering etc.
+            raw_data_address (string): As the user uploads the raw dataset it will be saved in the database, this is the address of that saved dataset.
+            target_variable (string): The user will select the target cloumn/variable and that target variable name have to be passed to the setup() in pycaret as patameter.
+            manual_preprocess_config_address(string): All the preprocessing configs passed by the user will be stored in a file and we extract the data from that file.
+        
+        Returns:
+            clean_data_address(string): 
+            
+        
+        """
+
+
+        if model_type == "classification":
+            clf1 = setup(data = raw_data_address, target = target_variable,silent=True, profile= True,manual_preprocess_config_address)
+        
+        elif model_type == "regression":
+            reg1 = setup(data = raw_data_address, target = target_variable,silent=True, profile= True,manual_preprocess_config_address)
+
+        # elif model_type == "clustering":            
+        #     clu1 = setup(data = raw_data_address,silent=True, profile= True,manual_preprocess_config_address)
+
+        # elif model_type == "nlp":            
+        #     nlp1 = setup(data = raw_data_address, target = target_variable,silent=True, profile= True,manual_preprocess_config_address)
+
+        X_train = get_config('X_train')
+        
+        X_train.to_csv('clean_data.csv', index=False)
+        
+        clean_data_address = os.getcwd()+"/clean_data.csv"
+        return clean_data_address
