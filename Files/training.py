9b8d2a8806ba47ca03a72e06233c4efec6bfd0d8
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-06-22 21:02:49 +0530

    started working on custom model creation auto.py is complete

--- Files/training.py
+++ Files/training.py
@@ -1,18 +1,15 @@
 from pycaret.classification import *
 from pycaret.regression import *
-from ray import tune 
+import json
+import yaml
+from yaml.loader import SafeLoader
 
 class training:
-    def __init__(self):
-        self.name=""
-    def top_models_auto(self,n): # n is the number of top models we need to return according to the user
-        top_nmodels=compare_models(n_select=n)
-        return top_nmodels
-
-    def top_models_custom(self,models_name_list): #names of the model in pycaret conventions
-        modellist=[]
-        for model_name in models_name_list:
-            new_model=create_model(model_name)
-            modellist.append(new_model)
-
-        return modellist
+    def training(self,model_array,Xdata,Ydata):
+        with open("model.yaml") as f:
+            model_uni= yaml.load(f,Loader=SafeLoader)
+        
+        data=json.loads(model_array)
+        for i in data:
+            if i["ishyper"]:
+                
