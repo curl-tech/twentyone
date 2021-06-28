153d8819c2da0fdc4e4816ff1889fd3c0655a559
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-06-28 15:19:46 +0530

    interim commit

--- Files/hyperparameter.py
+++ Files/hyperparameter.py
@@ -5,47 +5,48 @@ import pandas as pd
 import json
 import pandas as pd
 from .libraries import *
+from Files.metrics import Metrics as met
+class hyperparameter:
+    def optimize(model_str,modelname,userinputconfig,xdata,ydata):
+        """
+        This function in takes the string consisting of the name and the hyperparameters of the model and uses eval function to create the model.
+        Keylist is the dictionary consisting of the infomation about the user input ('subject to further changes')
+        Name is the name of the model selected (subject to future changes)
+        """
 
-def optimize(model_str,keylist,name,config):
-    """
-    This function in takes the string consisting of the name and the hyperparameters of the model and uses eval function to create the model.
-    Keylist is the dictionary consisting of the infomation about the user input ('subject to further changes')
-    Name is the name of the model selected (subject to future changes)
-    """
-    with open("config/model.yaml") as f:
-        model_uni= yaml.load(f,Loader=FullLoader)
+        xdata=pd.read_csv(xdata)
+        ydata=pd.read_csv(ydata)
+        with open(userinputconfig) as f:
+            userinputconfig= yaml.load(f,Loader=FullLoader)
 
-    with open("config/config.yaml") as new:
-        config= yaml.load(new,Loader=FullLoader)
-
-    x=pd.read_csv(config["xdata"])
-    y=pd.read_csv(config["ydata"])
-
-    params={}
-    for i in model_uni:
-        if i["name"]==name:
-            for hyper in i["hyper"]:
-                print(hyper)
-                if hyper["name"] not in keylist:
-                    if hyper["vary"]:
-                        if hyper["type"]=="options":
-                            params[hyper["name"]]=hyper["options"]
-                        elif hyper["type"]=="bool":
-                            params[hyper["name"]]=[True,False]
-                        elif hyper["type"]=="float" or hyper["type"]=="int":
-                            if hyper["range"]["type"]=="linear":
-                                if hyper["type"]=="int":
-                                    params[hyper["name"]]=(np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])).astype(int)
-                                else:
-                                    params[hyper["name"]]=np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])
-                            if hyper["range"]["type"]=="log":
-                                if hyper["type"]=="int":
-                                    params[hyper["name"]]=(np.logspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])).astype(int)
-                                else:
-                                    params[hyper["name"]]=(np.logspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"]))
-                    else:
-                        if hyper["type"]=="option":
-                            params[hyper["name"]]=hyper["options"]
-    model=eval(model_str)
-    clf=RandomizedSearchCV(model, params,verbose=10)
-    search=clf.fit(x,y)
+        params={}
+        for model in userinputconfig:
+            if model["isSelected"]:
+                modeltype=model["type"]
+                for hyper in model["hyper"]:
+                    if hyper["ischanged"]==False:
+                        if hyper["vary"]:
+                            if hyper["type"]=="options":
+                                params[hyper["name"]]=hyper["options"]
+                            elif hyper["type"]=="bool":
+                                params[hyper["name"]]=[True,False]
+                            elif hyper["type"]=="float" or hyper["type"]=="int":
+                                if hyper["range"]["type"]=="linear":
+                                    if hyper["type"]=="int":
+                                        params[hyper["name"]]=(np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])).astype(int)
+                                    else:
+                                        params[hyper["name"]]=np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])
+                                if hyper["range"]["type"]=="log":
+                                    if hyper["type"]=="int":
+                                        params[hyper["name"]]=(np.logspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])).astype(int)
+                                    else:
+                                        params[hyper["name"]]=(np.logspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"]))
+                        else:
+                            if hyper["type"]=="option":
+                                params[hyper["name"]]=hyper["options"]
+        model=eval(model_str)
+        clf=RandomizedSearchCV(model, params,verbose=10)
+        x_train,x_test,y_train,y_test=train_test_split(xdata,ydata,test_size=0.2)
+        clf.fit(x_train,y_train)
+        prediction=clf.predict(x_test)
+        met.calculate_metrics(modelname,modeltype,prediction,y_test)
--- Files/training.py
+++ Files/training.py
@@ -19,18 +19,26 @@ class training:
     def train(userinputconfig,modelconfig,xdata,ydata):
 
 
-        #b={"modeltype ":"classification", "modelname":"Logistic Regression","parameter":[{, "lr":73,"ishyper":True},{"modeltype":"classification", "modelname":"knn", "n_neighbours":3,"ishyper":"True"},{"modeltype ":"classification", "modelname":"decision trees", "ishyper":False}]}
+        
 
 
         with open(modelconfig) as f:
-            modelconfig= yaml.load(f,Loader=FullLoader)
+            modelconfig= yaml.load(f,Loader=FullLoader) #has info about where the data is stored and where the model must be stored
 
         with open(userinputconfig) as file:
             userinputconfig=yaml.load(file,Loader=FullLoader)
         models=[]
         ans=[]
+        
         for model in userinputconfig:
             if model["isSelected"]:
+                if model["type"]=='Classification':
+                    metrics=pd.DataFrame(columns = ['modelname','accuracy_score','recall_score','precision_score','f1_score','cohen_kappa_score','matthews_corrcoef'])
+                
+                elif model["type"]=='Regression':
+                    metrics=pd.DataFrame(columns=['modelname','mean_absolute_error','mean_squared_error','r2_score','mean_squared_log_error'])
+                
+
                 hypers=[]
                 keylist=[]
                 for feature in model["hyper"]:
@@ -39,4 +47,4 @@ class training:
                         hypers.append(feature["name"]+"="+ str(feature["value"]))
                 model_str=model["modelname"] + "(" + ", ".join(hypers) + ")"
     
-                hp.optimize(model_str,model["modelname"],userinputconfig)
+                metrics=hp.optimize(model_str,model["modelname"],userinputconfig,xdata,ydata,metrics)
