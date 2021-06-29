635fb1fc3d41e19c1bac3c821487e74d507d51f4
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-06-29 22:09:00 +0530

    did error handling in the metrics genenration to accomodate multi and binary classification

--- Files/metrics.py
+++ Files/metrics.py
@@ -1,3 +1,4 @@
+from numpy.core.numeric import NaN
 from sklearn.metrics import accuracy_score , recall_score, precision_score, f1_score, cohen_kappa_score, matthews_corrcoef
 from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,mean_squared_log_error
 
@@ -13,9 +14,16 @@ class Metrics:
 
         elif model_type=="regression":
             criteria=["mean_absolute_error","mean_squared_error","r2_score","mean_squared_log_error"]
-            
-        for criteria in criterias:
-            
-
         
+        metricsnewrow=[modelname]
+        for criteria in criterias:
+            criteria=eval(criteria)
+            try:
+                metricsnewrow.append(criteria(y,prediction))
+
+            except:
+                metricsnewrow.append(NaN)
+
+            metrics.loc[len(metrics.index)]=metricsnewrow
+            
         return metrics
