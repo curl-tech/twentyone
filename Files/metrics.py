0ad0599f6d896ff78ca03339ae567a8de6f8bbfd
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-06-29 21:18:21 +0530

    trying to optimize workflow in metrics generatio

--- Files/metrics.py
+++ Files/metrics.py
@@ -5,11 +5,17 @@ from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,mean
 class Metrics:
 
     def calculate_metrics(modelname,model_type,metrics,prediction,y): # nested list
+        
     
         if model_type=="classification":
-            metrics.loc[len(metrics.index)]=[modelname,accuracy_score(y,prediction),recall_score(y,prediction),precision_score(y,prediction),f1_score(y,prediction),cohen_kappa_score(y,prediction),matthews_corrcoef(y,prediction)]
+            criterias=["accuracy_score" , "recall_score", "precision_score", "f1_score", "cohen_kappa_score", "matthews_corrcoef"]
+           #metrics.loc[len(metrics.index)]=[modelname,accuracy_score(y,prediction),recall_score(y,prediction),precision_score(y,prediction),f1_score(y,prediction),cohen_kappa_score(y,prediction),matthews_corrcoef(y,prediction)]
 
         elif model_type=="regression":
-            metrics.loc[len(metrics.index)]=[modelname,mean_absolute_error(y,prediction),mean_squared_error(y,prediction),r2_score(y,prediction),mean_squared_log_error(y,prediction)]
+            criteria=["mean_absolute_error","mean_squared_error","r2_score","mean_squared_log_error"]
+            
+        for criteria in criterias:
+            
+
         
         return metrics
