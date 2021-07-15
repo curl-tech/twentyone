c9599ff816c147030a69ebfc311731e432cdea72
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-07-15 16:21:27 +0530

    yaml file fixed and working and tested

--- Files/hyperparameter.py
+++ Files/hyperparameter.py
@@ -8,15 +8,18 @@ import pandas as pd
 from .libraries import *
 from Files.metrics import Metrics as met
 class hyperparameter:
-    def optimize(model_str,modelname,userinputconfig,xdata,ydata,metrics,dataconfig):
+    def optimize(model_str,modelname,userinputconfig,data,dataconfig,target_column):
         """
         This function in takes the string consisting of the name and the hyperparameters of the model and uses eval function to create the model.
         Keylist is the dictionary consisting of the infomation about the user input ('subject to further changes')
         Name is the name of the model selected (subject to future changes)
         """
 
-        xdata=pd.read_csv(xdata)
-        ydata=pd.read_csv(ydata)
+        data=pd.read_csv(data)
+        ydata=data[target_column]
+        data.drop([target_column],inplace=True,axis=1)
+        xdata=data
+
         with open(userinputconfig) as f:
             userinputconfig= yaml.load(f,Loader=FullLoader)
 
@@ -25,8 +28,11 @@ class hyperparameter:
 
         params={}
         for model in userinputconfig:
-            if model["isSelected"]:
-                modeltype=model["type"]
+            if model["name"]==modelname:
+                
+                modelname=model["name"]
+                print(modelname)
+                model_type=model["type"]
                 for hyper in model["hyper"]:
                     if hyper["ischanged"]==False:
                         if hyper["vary"]:
@@ -39,22 +45,25 @@ class hyperparameter:
                                     if hyper["type"]=="int":
                                         params[hyper["name"]]=(np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])).astype(int)
                                     else:
+                                        print(hyper)
                                         params[hyper["name"]]=np.linspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])
                                 if hyper["range"]["type"]=="log":
                                     if hyper["type"]=="int":
-                                        params[hyper["name"]]=(np.logspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"])).astype(int)
+                                        params[hyper["name"]]=(np.logspace(np.log10(hyper["range"]["min"]),np.log10(hyper["range"]["max"]),hyper["range"]["num_samp"])).astype(int)
                                     else:
-                                        params[hyper["name"]]=(np.logspace(hyper["range"]["min"],hyper["range"]["max"],hyper["range"]["num_samp"]))
+                                        params[hyper["name"]]=(np.logspace(np.log10(hyper["range"]["min"]),np.log10(hyper["range"]["max"]),hyper["range"]["num_samp"]))
                         else:
                             if hyper["type"]=="option":
                                 params[hyper["name"]]=hyper["options"]
         model=eval(model_str)
         clf=RandomizedSearchCV(model, params,verbose=10)
         x_train,x_test,y_train,y_test=train_test_split(xdata,ydata,test_size=0.2)
+        print("working on "+ modelname)
         clf.fit(x_train,y_train)
-
-        
-        pickle.dump(clf)
+        print("model completed for " + modelname)
+        picklepath=os.path.join(dataconfig["location"],(str(modelname) +".pkl"))
+        with open(picklepath,"wb") as f:
+            pickle.dump(clf,f)
         prediction=clf.predict(x_test)
-        metrics=met.calculate_metrics(modelname,modeltype,metrics,prediction,y_test)
-        return metrics
+        metricsrow=met.calculate_metrics(modelname,model_type,prediction,y_test)
+        return metricsrow
--- Files/metrics.py
+++ Files/metrics.py
@@ -5,25 +5,24 @@ from sklearn.metrics import mean_absolute_error,mean_squared_error,r2_score,mean
 
 class Metrics:
 
-    def calculate_metrics(modelname,model_type,metrics,prediction,y): # nested list
-        
-    
-        if model_type=="classification":
-            criterias=["accuracy_score" , "recall_score", "precision_score", "f1_score", "cohen_kappa_score", "matthews_corrcoef"]
-           #metrics.loc[len(metrics.index)]=[modelname,accuracy_score(y,prediction),recall_score(y,prediction),precision_score(y,prediction),f1_score(y,prediction),cohen_kappa_score(y,prediction),matthews_corrcoef(y,prediction)]
+    def calculate_metrics(modelname,model_type,prediction,y): # nested list
 
-        elif model_type=="regression":
-            criteria=["mean_absolute_error","mean_squared_error","r2_score","mean_squared_log_error"]
-        
+
+        if model_type=="Classification":
+            criterias=["accuracy_score" , "recall_score", "precision_score", "f1_score", "cohen_kappa_score", "matthews_corrcoef"]
+        #metrics.loc[len(metrics.index)]=[modelname,accuracy_score(y,prediction),recall_score(y,prediction),precision_score(y,prediction),f1_score(y,prediction),cohen_kappa_score(y,prediction),matthews_corrcoef(y,prediction)]
+
+        elif model_type=="Regression":
+            criterias=["mean_absolute_error","mean_squared_error","r2_score","mean_squared_log_error"]
+
         metricsnewrow=[modelname]
         for criteria in criterias:
-            criteria=eval(criteria)
             try:
-                metricsnewrow.append(criteria(y,prediction))
+                metricsnewrow.append(eval(criteria+"(y,prediction)"))
 
             except:
-                metricsnewrow.append(NaN)
-
-            metrics.loc[len(metrics.index)]=metricsnewrow
+                metricsnewrow.append("Not Available")
             
-        return metrics
+        
+
+        return metricsnewrow
