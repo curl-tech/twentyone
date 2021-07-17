8bfa9edd864057e137c79a9d745233771dca2e66
WrathTitan <rishabhbhatt159@gmail.com>
2021-07-17 16:11:25 +0530

    Shell files updated with interrupt handling capability

--- application-startup-ubuntu.sh
+++ application-startup-ubuntu.sh
@@ -1,13 +1,29 @@
 #!/bin/sh
+
+#The below function will be called in case Ctrl+C is pressed, i.e, Interrupt
+terminationFunction () {
+    trap INT                    #Restore signal handling to previous before exit.
+    echo ''
+    echo 'Exiting... MongoDB Server Being Closed...'    # Printing Message
+    sudo systemctl stop mongod
+    echo 'Exited.'
+    exit                        #To exit the script
+}
+
+# Set up SIGINT trap to call terminationFunction.
+trap "terminationFunction" INT
+
 #To Start MongoDB Server
 sudo systemctl start mongod
+
 #To Activate the Virtual Environment
-source venv/bin/activate
-#To Start the FastAPI server
-python3 api.py
-#Moving into the frontend directory
-cd Frontend/pr21/
-#Installing the required node dependencies for react
-#npm i
-#Starting the react server
-#npm start
+. venv/bin/activate
+
+#To Start React - Frontend Server
+/bin/sh -ec 'cd Frontend/pr21 && npm start &'
+
+#To Start FastAPI - Backend Server
+/bin/sh -ec 'python3 api.py'
+
+# Restore signal handling to previous before exit.
+trap INT
