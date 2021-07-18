46322b76e451afb57f97561c426afc13079b1a6e
PaarthBarkur1 <paarthbarkur@gmail.com>
2021-07-18 20:34:25 +0530

    added time series

--- Files/timeseries.py
+++ Files/timeseries.py
@@ -1,3 +1,72 @@
 from pmdarima import auto_arima
+from fbprophet import Prophet
+import json
+from fbprophet.serialize import model_to_json, model_from_json
+import os
+import yaml
+from yaml.loader import FullLoader
+import plotly
+import pandas as pd
+import plotly.express as ex
+
+from Files.metrics import Metrics as met
 class timeseries:
-    def train()
+    def createprophet(dataconfig):
+        with open(dataconfig) as f:
+            dataconfigfile= yaml.load(f,Loader=FullLoader)
+        data=dataconfig["data"]
+        location=dataconfig["location"]
+        model=Prophet()
+        testsize=int(len(data)*0.2)
+        train=data.iloc[:-testsize]
+        test=data.iloc[-testsize:]
+        model.fit(train)
+        pred=model.predict(test)
+        pred=pred.yhat
+        actual=test.y
+
+        metrics=met.calculate_metrics("fbprophet","Regression",pred,actual)
+        metricsLocation=os.path.join(dataconfigfile["location"],"metrics.csv")
+        metrics.to_csv(metricsLocation, index=True)
+
+        compare=pd.DataFrame(pred.values,columns=['predictions'])
+        compare['actual']=actual.values
+        print(compare)
+        fig=compare.plot(legend=True)
+        plotly.offline.plot(fig,filename=os.path.join(location,"fbtestvspred.html"))
+
+        modelfinal=Prophet()
+        modelfinal.fit(data)
+        location="serialized_model.json"
+        location=os.path.join(dataconfigfile['location'],str(dataconfigfile['projectname'])+str('fb'))
+        with open(location, 'w') as fout: #save the model
+            json.dump(model_to_json(modelfinal), fout)
+        return location
+
+    def fbinference(location,number):
+        with open(location, 'r') as fin:
+            model = model_from_json(json.load(fin))
+        future=model.make_future_dataframe(periods=number)
+        pred=model.predict(future)
+        return pred
+
+    def createarima(dataconfig):
+        with open(dataconfig) as f:
+            dataconfigfile= yaml.load(f,Loader=FullLoader)
+
+        data=dataconfig["data"]
+        location=dataconfig["location"]
+        testsize=int(len(data)*0.2)
+        train=data.iloc[:-testsize]
+        test=data.iloc[-testsize:]
+        model = auto_arima(train['y'],trace=True) 
+        testpred=model.predict(testsize)
+        testactual=test.y
+
+        metrics=met.calculate_metrics("fbprophet","Regression",testpred,testactual)
+        compare=pd.DataFrame(testpred,columns=['predictions'])
+        compare['actual']=testactual.values
+
+        fig=compare.plot(legend=True)
+        plotly.offline.plot(fig,filename=os.path.join(location,"arimatestvspred.html"))
+        modelfinal=auto_arima(data['y'], trace=True,suppress_warnings=True)
--- Files/training.py
+++ Files/training.py
@@ -8,16 +8,15 @@ from .libraries import *
 import yaml
 from yaml.loader import FullLoader
 from Files.hyperparameter import hyperparameter as hp
-import os
-
+import os 
 
 class training:
 
     def train(userinputconfig,dataconfig,preprocessconfig):
-
+        
         with open(preprocessconfig) as f:
             preprocessconfigfile= yaml.load(f,Loader=FullLoader) #for split ratio
-        
+
 
         with open(dataconfig) as f:
             dataconfigfile= yaml.load(f,Loader=FullLoader) #has info about where the data is stored and where the model must be stored
@@ -27,21 +26,22 @@ class training:
         models=[]
         ans=[]
 
-        test_ratio=preprocessconfigfile["split_ratio_test"] #input given the the user usually 30 by default
-        
-        xdata=dataconfigfile["Xdata"] #the location of the x data
+        test_ratio=preprocessconfigfile["split_ratio_test"] #input given the the user usually 0.3 by default
 
-        ydata=dataconfigfile["Ydata"] #the location of the y data
+        data=dataconfigfile["data"] 
         
+        target_column=preprocessconfigfile["target_column_name"]
+        
+        
+        if dataconfigfile["problem_type"]=='classification':
+            metrics=pd.DataFrame(columns = ['modelname','accuracy_score','recall_score','precision_score','f1_score','cohen_kappa_score','matthews_corrcoef'])
+
+        elif dataconfigfile["problem_type"]=='regression':
+            metrics=pd.DataFrame(columns=['modelname','mean_absolute_error','mean_squared_error','r2_score','mean_squared_log_error'])
+
         #creates a pandas dataframe to store the metrics of the created model
         for model in userinputconfigfile:
             if model["isSelected"]:
-                if model["type"]=='Classification':
-                    metrics=pd.DataFrame(columns = ['modelname','accuracy_score','recall_score','precision_score','f1_score','cohen_kappa_score','matthews_corrcoef'])
-                
-                elif model["type"]=='Regression':
-                    metrics=pd.DataFrame(columns=['modelname','mean_absolute_error','mean_squared_error','r2_score','mean_squared_log_error'])
-                
 
                 hypers=[]
                 keylist=[]
@@ -49,10 +49,12 @@ class training:
                     if feature["ischanged"]:
                         keylist.append(feature["name"])
                         hypers.append(feature["name"]+"="+ str(feature["value"]))
-                model_str=model["modelname"] + "(" + ", ".join(hypers) + ")"
-    
-                metrics=hp.optimize(model_str,model["modelname"],userinputconfig,xdata,ydata,metrics,dataconfig)
+                model_str=model["name"] + "(" + ", ".join(hypers) + ")"
 
+                metricsnewrow=hp.optimize(model_str,model["name"],userinputconfig,data,dataconfig,target_column)
+                print(metricsnewrow)
+                metrics.loc[len(metrics.index)]=metricsnewrow
+                
         #stores the metrics in the assigned folder       
-        metricsLocation=os.path.join(dataconfig["location"],"metrics.csv")
+        metricsLocation=os.path.join(dataconfigfile["location"],"metrics.csv")
         metrics.to_csv(metricsLocation, index=True, index_label="modelname")
